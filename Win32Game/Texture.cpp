8a831091a81c29b7ffb23e4901cbb25498780050
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-05-08 11:40:24 +0900

    DC가 nullptr인 상태에서 BITMAP속성을 가져오는 오류를 수정합니다.

--- Win32Game/Texture.cpp
+++ Win32Game/Texture.cpp
@@ -1,4 +1,5 @@
 #include "Texture.h"
+#include "RenderSystem.h"
 
 Texture::Texture() : _DC(0), _Bitmap(0), _BitInfo{}
 {
@@ -13,6 +14,7 @@ Texture::~Texture()
 void Texture::Load(const std::wstring& filepath)
 {
 	//TODO: 여기에 이미지를 로드해 갖고오는 코드를 만들어야 합니다.
+	_DC = CreateCompatibleDC(RenderSystem::GetInstance()->_hdc);
 	_Bitmap = (HBITMAP)LoadImage(nullptr, filepath.c_str(), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_LOADFROMFILE);
 	GetObject(_Bitmap, sizeof(BITMAP), &_BitInfo);
 
